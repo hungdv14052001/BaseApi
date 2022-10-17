8d38066001ac6c7c3d15545ce9a852c03af47e2c
hungdv14052001 <hung.dangvan@devfast.us>
2022-10-17 10:12:55 +0700

    add authenticate

---
+++ BaseApi/Seeder/UserSeeder.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BaseApi.Models;
+using BaseApi.Common;
+
+namespace BaseApi.Seeder
+{
+    class UserSeeder
+    {
+        private readonly ModelBuilder _modelBuilder;
+        public UserSeeder(ModelBuilder modelBuilder)
+        {
+            _modelBuilder = modelBuilder;
+        }
+
+        /// <summary>
+        /// Excute data
+        /// </summary>
+        public void SeedData()
+        {
+            _modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    Id = 1,
+                    Name = "Đặng Văn Hùng",
+                    Username = "DangVanHung",
+                    Password = Untill.CreateMD5("DangVanHung"),
+                    Role = "Admin"
+                });
+        }
+    }
+}
